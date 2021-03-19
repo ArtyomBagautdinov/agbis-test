8a382daaad35df82e2d587f4304a081c1a604202
ArtyomBagautdinov <bagautdinovartyom@gmail.com>
2021-03-19 11:01:56 +0200

    продолжение работы над вторым заданием

--- src/views/Posts.vue
+++ src/views/Posts.vue
@@ -12,9 +12,18 @@
             </div>
         </div>
         <div>
-            <div @touchstart="handleEventStart" @touchend="handleEventEnd" class="w-full z-10 bg-white shadow-navbar fixed bottom-0 transition-all duration-500" :class="{'h-32': !isOpen, 'h-4/5': isOpen}">
+            <div @touchstart="handleEventStart" @touchend="handleEventEnd" class="w-full z-10 bg-white shadow-navbar fixed bottom-0 transition-all duration-500" :class="{'h-44': !isOpen, 'h-4/5': isOpen}">
+                <div class="flex flex-row justify-center items-start transition-all duration-100" :class="{'opacity-0': isOpen}">
+                    <div class="w-1/3 p-2 flex flex-row justify-end items-center">
+                        <img src="@/assets/images/tables.svg" alt="">
+                    </div>
+                    <div class="flex flex-col w-2/3">
+                        <span class="font-bold swipe-bar-color">Смарт таблицы</span>
+                        <span class="font-medium swipe-bar-color text-xs">В данном сервисе представлены инструменты для мониторинга эффективности вашего бизнеса</span>
+                    </div>
+                </div>
             </div>
-            <div  class="w-full z-20 fixed bottom-0 h-20 bg-white  text-black flex flex-row justify-around items-center">
+            <div class="w-full z-20 fixed bottom-0 h-20 bg-white  text-black flex flex-row justify-around items-center px-2">
                 <div v-for="val in tabBarItems" :key="val.id" class="flex flex-col items-center justify-center cursor-pointer">
                     <img class="h-7 w-7 " :src="val.img" :alt="val.name">
                     <span class="text-xs tab-bar-color font-medium">{{val.name}}</span>
